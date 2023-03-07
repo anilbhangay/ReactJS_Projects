de689ed28294c22578db0e331a3a4e98f8b1e813
anilbhangay <anilbhangay@gmail.com>
2023-03-07 23:59:49 +0530

    fixed some error in js

--- pw_skills/src/Components/Fame.js
+++ pw_skills/src/Components/Fame.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 function Fame({fame}){
   return (
@@ -7,11 +7,15 @@ function Fame({fame}){
             <img src={fame} alt='fameImage' className='fame-image mt-4'></img>
             <h5 className="fame-name mt-3">Anil Bhangay</h5>
             <p className="fame-position fw-semibold text-secondary">iNeuron, FullStack WebDeveloper Aspirant</p>
-
+            <p className="fame-feed pt-3">Hi All, I am a student of the NOV 2022 batch, 
+                I made a good decision by joining the Ineuron classes. 
+                A very big thank you</p>
+                <div className="pb-4">
+                    <i className="fa-brands fa-linkedin fa-2x"></i>
+                </div>
         </div>
-
      </div>
   )
 }
 
-export default Fame
+export default Fame;
