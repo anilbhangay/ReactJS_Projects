61b7cb6b356c1233dc53994f62646d848ef1875d
anilbhangay <anilbhangay@gmail.com>
2023-03-08 19:38:35 +0530

    Added new program in js file

--- pw_skills/src/Components/Product.js
+++ pw_skills/src/Components/Product.js
@@ -1,17 +1,16 @@
-import React from 'react';
+import React from "react";
 
 function Product(props){
-  return (
-    <div className='col mt-4'>
-        <div className='p-3 product-cart text-center'>
-            <img src={props.image} alt="productImage" className='product-image mt-4'></img>
-            <h4 className="product-name mt-3 fw-bold">{props.productName}</h4>
-            <p className="product-info pt-3 text-secondary fs-5">
-            {props.productInfo}
+    return(
+        <div className="col mt-4">
+            <div className="p-3 product-cart text-center">
+                <img src={props.image} alt="productImage" className="product-image mt-4"></img>
+                <h4 className="product-name mt-3 fw-bold">{props.productName}</h4>
+                <p className="product-info pt-3 text-secondary fs-5">
+                {props.productInfo}
                 </p>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
-
-export default Product;
+export default Product
---
+++ pw_skills/src/Components/Program.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import ProgramCart from './ProgramCart'
+
+function Program (props){
+  return (
+    <>
+    <div className='row'>
+      <div className='col text-center'>
+           <h1 className='program-heading'>{props.programHeading}</h1>
+           <p className='program-dailog text-secondary fs-5'>{props.programDialog}</p>
+      </div>
+    </div>
+      <div className='row mt-5 gx-5'>
+        <ProgramCart image={props.Image} courseName={"Full Stack web development"} 
+          mentor={"Hitesh Choudhary"} price={"2975"}/>
+        <ProgramCart image={props.Image} courseName={"Full Stack web development"} 
+          mentor={"Hitesh Choudhary"} price={"2975"}/>
+        <ProgramCart image={props.Image} courseName={"Full Stack web development"} 
+          mentor={"Hitesh Choudhary"} price={"2975"}/>
+      </div>
+      </>
+  )
+}
+
+export default Program
