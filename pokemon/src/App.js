18b4000caf1f2fed350b1dd267ce01db0c62ba2f
anilbhangay <anilbhangay@gmail.com>
2023-03-10 12:36:01 +0530

    Added new file css

--- pokemon/src/App.js
+++ pokemon/src/App.js
@@ -3,6 +3,14 @@ import './App.css';
 function App() {
   return (
     <>
+    <div className='Navbar'>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Support</li>
+          <li>Contact</li>
+        </ul>
+    </div>
     
     </>
   );
