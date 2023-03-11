fdf70080d8115a2e96ddd5aef0d702055bda4c35
anilbhangay <anilbhangay@gmail.com>
2023-03-11 18:57:48 +0530

    fixed error in js file

--- pokemon/src/App.js
+++ pokemon/src/App.js
@@ -3,15 +3,7 @@ import './App.css';
 function App() {
   return (
     <>
-    <div className='Navbar'>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Support</li>
-          <li>Contact</li>
-        </ul>
-    </div>
-    
+      
     </>
   );
 }
