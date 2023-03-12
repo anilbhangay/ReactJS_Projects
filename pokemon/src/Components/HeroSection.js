8d6d4b487a033f37817869cc16a7b1e692d2287b
anilbhangay <anilbhangay@gmail.com>
2023-03-12 23:37:58 +0530

    Added createContext in js files

---
+++ pokemon/src/Components/HeroSection.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import UserCard from './UserCard';
+
+const HeroSection = () => {
+  return (
+    <div>
+        <img  src='https://tinypng.com/images/social/website.jpg' alt='random-img'/>
+        <UserCard />
+    </div>
+  )
+}
+
+export default HeroSection;
