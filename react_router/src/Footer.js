56e2b9704ee6584ab751baf98a423baf8cefa0c1
anilbhangay <anilbhangay@gmail.com>
2023-03-19 00:32:43 +0530

    Added style in all files

--- react_router/src/Footer.js
+++ react_router/src/Footer.js
@@ -3,7 +3,15 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='footers'>
-        <footer>@Copyright 2023 By Anil Kumar</footer> 
+        <p>@Copyright 2023 By Anil Kumar
+      
+        <i class="fa-brands fa-facebook"></i>
+        <i class="fa-brands fa-square-twitter"></i>
+        <i class="fa-brands fa-github"></i>
+        <i class="fa-brands fa-square-instagram"></i>
+        <i class="fa-brands fa-linkedin"></i>
+        </p> 
+      
     </div>
   )
 }
