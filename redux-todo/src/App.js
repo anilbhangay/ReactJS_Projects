8ef63adc270349236a39d5266305aa5769d9c13e
anilbhangay <anilbhangay@gmail.com>
2023-04-07 23:38:30 +0530

    Changed some para and header in app

--- redux-todo/src/App.js
+++ redux-todo/src/App.js
@@ -4,10 +4,14 @@ import './App.css';
 const App = () => {
   return (
     <>
-     <h1>This is a Todo App</h1>
-
+     <h1>Todo List</h1>
+     <div>
+      <input type='{name}' placeholder='Type Here'></input>
+      <button>Add</button>
+     </div>
+       
     </>
   )
 }
 
-export default App
+export default App;
