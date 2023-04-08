075601ee82c5378cbe243d83d9dacc6576cafc85
anilbhangay <anilbhangay@gmail.com>
2023-04-08 23:41:22 +0530

    Fixed some errors in css file

--- redux-todo/src/App.js
+++ redux-todo/src/App.js
@@ -4,12 +4,7 @@ import './App.css';
 const App = () => {
   return (
     <>
-     <h1>Todo List</h1>
-     <div>
-      <input type='{name}' placeholder='Type Here'></input>
-      <button>Add</button>
-     </div>
-       
+     <h1>Todo List</h1>  
     </>
   )
 }
