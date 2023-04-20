eefc4b40c1a3bc54d9a30182c05572f230dfd624
anilbhangay <anilbhangay@gmail.com>
2023-04-20 23:52:56 +0530

    Edited some mistake in files

--- redux-todo/src/Redux/Store.js
+++ redux-todo/src/Redux/Store.js
@@ -4,7 +4,10 @@ import React from 'react'
 const Store = () => {
   return (
     <>
-    
+    <div>
+      <input className='search'>Add Here</input>
+      <button>Add</button>
+    </div>
     </>
   )
 }
