fbee9bd8bce4734c9a7590955b80367e4dbaabc9
anilbhangay <anilbhangay@gmail.com>
2023-04-20 23:48:07 +0530

    Changed header in app

--- redux-todo/src/App.js
+++ redux-todo/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 const App = () => {
   return (
     <>
-     <h1>Todo App</h1>
+     <h1>Todo list</h1>
      <div>
       <input type='{name}' placeholder='Type Here'></input>
       <button>Add</button>
