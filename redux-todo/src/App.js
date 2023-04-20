d026c2e12d2d8083d42e59e2403ed9a1639c78b0
anilbhangay <anilbhangay@gmail.com>
2023-04-20 23:56:16 +0530

    Fixed some error in app file

--- redux-todo/src/App.js
+++ redux-todo/src/App.js
@@ -4,12 +4,11 @@ import './App.css';
 const App = () => {
   return (
     <>
-     <h1>Todo list</h1>
+     <h1>Todo App</h1>
      <div>
       <input type='{name}' placeholder='Type Here'></input>
       <button>Add</button>
      </div>
-       
     </>
   )
 }
--- redux-todo/src/Redux/Store.js
+++ redux-todo/src/Redux/Store.js
@@ -4,10 +4,7 @@ import React from 'react'
 const Store = () => {
   return (
     <>
-    <div>
-      <input className='search'>Add Here</input>
-      <button>Add</button>
-    </div>
+   
     </>
   )
 }
