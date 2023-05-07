21f2e6bd75f79309822febbfe42924937341c646
anilbhangay <anilbhangay@gmail.com>
2023-05-07 23:10:54 +0530

    Changed Effects on App

--- redux-todo/src/App.js
+++ redux-todo/src/App.js
@@ -5,12 +5,14 @@ const App = () => {
   return (
     <>
      <h1>Todo App</h1>
-     <div>
+     <div className='search'>
       <input type='{name}' placeholder='Type Here'></input>
       <button>Add</button>
       </div>
        <div className='todo'>
-        <h1>This is a todo app</h1>
+         <p>
+          this is a Todo App List
+         </p>
         </div>  
     </>
   )
--- redux-todo/src/Redux/Store.js
+++ redux-todo/src/Redux/Store.js
@@ -4,7 +4,9 @@ import React from 'react'
 const Store = () => {
   return (
     <>
-   
+      <div className='store'> 
+          
+      </div>
     </>
   )
 }
