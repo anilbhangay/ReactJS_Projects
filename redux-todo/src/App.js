ccfcac137919493fd2f7d91ec239ae9b4675403f
anilbhangay <anilbhangay@gmail.com>
2023-05-10 23:34:15 +0530

    Added button in script file

--- redux-todo/src/App.js
+++ redux-todo/src/App.js
@@ -9,7 +9,13 @@ const App = () => {
       <input type='{name}' placeholder='Type Here'></input>
       <button>Add</button>
       </div>
-
+           
+         <div className='read'>
+          <p> 
+             this is unique code of todo app list
+          </p>
+          <button>Delete</button>
+          </div>  
     </>
   )
 }
