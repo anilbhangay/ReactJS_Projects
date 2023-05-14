d66f49e2398167ba14aa02684b9f62b71b32fe9f
anilbhangay <anilbhangay@gmail.com>
2023-05-14 20:32:39 +0530

    CreateSlice  in  todoSlice js file

---
+++ redux-todo/src/Features/todoSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+
+}
+
+const todoSlice = createSlice({
+  name: 'todos',
+  initialState,
+  reducers: {
+
+  }
+});
+
+export const {
+
+} = todoSlice.actions
+
+export default todoSlice.reducer
