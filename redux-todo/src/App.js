5fb4a9abb3a5f2f814cd72446858c48aa196359d
anilbhangay <anilbhangay@gmail.com>
2023-05-16 20:04:38 +0530

    Added todoList in App.js file

--- redux-todo/src/App.js
+++ redux-todo/src/App.js
@@ -2,11 +2,25 @@ import React from 'react';
 import './App.css';
 import Input from './Components/Input';
 
+const todoList = [{
+  item: 'todo',
+  done: false,
+  id: 695426487923
+},{
+    item:  'todo2',
+    done:  true,
+    id:   85838
+}]
+
 function App() {
     return (
-       <div className='App'>
-        
+       <div className='app'>
+         <div className='app_container'>
+            <div className='app_todoContainer'>
+              
+            </div>
          <Input />
+         </div>
        </div>
     );
 }
