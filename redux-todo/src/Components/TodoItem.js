a50f78f4a3b6806781c6d18976920e9b0e50604a
anilbhangay <anilbhangay@gmail.com>
2023-05-17 19:21:53 +0530

    Updated in todoiitem

--- redux-todo/src/Components/TodoItem.js
+++ redux-todo/src/Components/TodoItem.js
@@ -4,7 +4,7 @@ const TodoItem = ({ name, done, id }) => {
   return (
     <div className='todoItem'>
 
-      <p className={done && 'todoItem--'}>{name}</p>
+      <p className={done && 'todoItem--done'}>{name}</p>
     </div>
   )
 }
